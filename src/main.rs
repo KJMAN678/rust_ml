859345d66f954b83381fdc9fdb155a3c52eeade1
KJMAN678 <kj.fireman5@gmail.com>
2022-03-14 23:12:47 +0900

    機械学習用の実装を追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
-extern crate hello;
+use smartcore::dataset::boston;
+use smartcore::linalg::naive::dense_matrix::DenseMatrix;
+use smartcore::model_selection::train_test_split;
+use smartcore::linear::linear_regression::{LinearRegression, LinearRegressionParameters, LinearRegressionSolverName};
+use smartcore::metrics::mean_squared_error;
 
 fn main() {
-  let word:&str = "World";
-  println!("{:?}", hello::hello_world(word));
+
+  let boston_data = boston::load_dataset();
+
+  let x = DenseMatrix::from_array(
+    boston_data.num_samples,
+    boston_data.num_features,
+    &boston_data.data,
+  );
+
+  let y = boston_data.target;
+
+  let (x_train, x_test, y_train, y_test) = train_test_split(
+                                                    &x,    // training data
+                                                    &y,    // target
+                                                    0.2,   // test_size
+                                                    false, // shuffle
+                                                  );
+
+  let model = LinearRegression::fit(
+                            &x_train, 
+                            &y_train,
+                            LinearRegressionParameters::default().
+                              with_solver(LinearRegressionSolverName::QR)
+                            ).unwrap();
+
+  let y_pred = model.predict(&x_test).unwrap();
+
+  let metrics = mean_squared_error(&y_test, &y_pred);
+
+  println!("mse:{}", metrics);
+
 }
