82fdf70dbf9b9c0e0e78d3da392afc1e693aded3
KJMAN678 <kj.fireman5@gmail.com>
2022-03-14 23:26:45 +0900

    機械学習と自作ライブラリを併存させた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate hello;
+
 use smartcore::dataset::boston;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::model_selection::train_test_split;
@@ -5,24 +7,32 @@ use smartcore::linear::linear_regression::{LinearRegression, LinearRegressionPar
 use smartcore::metrics::mean_squared_error;
 
 fn main() {
+  // 自作クレート
+  let word:&str = "World";
+  println!("{:?}", hello::hello_world(word));
 
+  // 機械学習
+  // データセットのダウンロード
   let boston_data = boston::load_dataset();
 
+  // 特徴量を arrayデータへ変換
   let x = DenseMatrix::from_array(
     boston_data.num_samples,
     boston_data.num_features,
     &boston_data.data,
   );
 
+  // 目的変数
   let y = boston_data.target;
 
+  // 評価データと訓練データに分割
   let (x_train, x_test, y_train, y_test) = train_test_split(
                                                     &x,    // training data
                                                     &y,    // target
                                                     0.2,   // test_size
                                                     false, // shuffle
                                                   );
-
+  // 学習
   let model = LinearRegression::fit(
                             &x_train, 
                             &y_train,
@@ -30,10 +40,12 @@ fn main() {
                               with_solver(LinearRegressionSolverName::QR)
                             ).unwrap();
 
+  // 推論
   let y_pred = model.predict(&x_test).unwrap();
 
+  // 評価
   let metrics = mean_squared_error(&y_test, &y_pred);
 
+  // 評価結果を出力
   println!("mse:{}", metrics);
-
 }
